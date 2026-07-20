8bc2138ac438a95ce493ca1f953e4bc121b8c25a
Denish Bhandari <159867533+denish233@users.noreply.github.com>
2026-07-20 23:05:25 +0000

    assignment

---
+++ module_05/1.py
@@ -0,0 +1,35 @@
+import csv
+
+with open("dict.csv","r")as file:
+    reader=csv.reader(file)
+
+    first_line=next(reader)
+    secondline=next(reader)     
+print(first_line)
+print(secondline)
+ 
+pirate_dict = {
+    "hello": "avast",
+    "exuse": "arr",
+    "sir": "matey",
+    "your": "yer"
+}
+
+print(pirate_dict)
+
+with open("in.txt","r") as file:
+           text = file.read()
+print(text)
+
+
+translated_text = " ".join(
+    map(lambda word: pirate_dict.get(word, word), text.split())
+)
+
+# Write output file
+with open("out.txt", "w") as file:
+    file.write(translated_text)
+
+print("Translation complete!")
+
+
